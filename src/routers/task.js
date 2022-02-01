821b5309e425d252b7ed838ab26cd11d41d0d540
KUESTER <kuester@de.ibm.com>
2022-02-01 14:37:53 +0100

    Finish lesson 101: separate route files

---
+++ src/routers/task.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const Task = require('../models/task')
+const router = new express.Router()
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+    
+        // res.status(400)  // If user error occurs, status code is set to 400: Bad request.
+        // See https://httpstatuses.com for all statuses available.
+        // res.status must be called before sending the error!
+        // res.send(error)
+        // This can be chained together:
+})
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = [ 'description', 'completed']
+    const isAllowedUpdate = updates.every((update) => allowedUpdates.includes(update))
+    if (!isAllowedUpdate) {
+        res.status(400).send('Invalid task update')
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+
+})
+module.exports = router
